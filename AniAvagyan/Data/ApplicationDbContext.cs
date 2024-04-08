87a13f7b7290d19b6a5afd642b6459c332972cb8
Ani <Ani@DESKTOP-15E8FHN>
2024-04-08 17:12:48 +0400

    Add project files.

---
+++ AniAvagyan/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AniAvagyan.Models.Entities;
+
+namespace AniAvagyan.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {    
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
